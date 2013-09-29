4b3d744e4f88fb33960fb30ec98a40367ebafe0a
atulonruby <atuljha86@gmail.com>
2013-09-29 02:32:17 -0700

    admin for pic

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Picture do
-  
+  show do |ad|
+        attributes_table do
+          row :id
+          row :image do
+            image_tag(ad.image.url,:size => "500x500")
+          end
+          row :width
+          row :height
+          row :description
+          row :created_at
+          row :updated_at
+          
+        end
+        active_admin_comments
+      end
 end
