c3b83b94e872d73af0629ac82fb23fd00e2ec484
atulonruby <atuljha86@gmail.com>
2014-04-14 17:12:20 -0700

    Set up info@electricoak.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default :to => "atuljha86@gmail.com" 
+  default :to => "info@electricoak.com" 
    def registration_confirmation(msg)
      @message = msg
-     mail( :subject => "Contact from ElectricOak Site", :from => "ajvictoria03@gmail.com")
+     mail( :subject => "Contact from ElectricOak Site", :from => default_from(msg), :cc => mail_list)
+   end
+   
+   :private
+   def mail_list
+     "atuljha86@gmail.com, alec.myring@gmail.com, bobgecs@gmail.com, godfrey.ben@gmail.com"
+   end
+   
+   def default_from(msg)
+     msg.email.blank? ? "eletricoak.com" : msg.email
    end
 end
